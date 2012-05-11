1ab9a38c872c9a60f2adcbe3eb3e4ecefe772db3
Eric Rannaud <e@nanocritical.com>
2012-05-11 12:54:45 -0700

    nlang.postlude: say and print should take the address of the string instead
    No reason to have copies like that everywhere; let's take advantage of
    N's ability to take addresses of temporaries.

--- nlang_site/nlang/postlude.h
+++ nlang_site/nlang/postlude.h
@@ -4,13 +4,13 @@ void nlang_postlude_abort() {
   abort();
 }
 
-void nlang_postlude_print(nlang_stringmod_string s) {
-  const char *cstr = (const char *) nlang_stringmod_string_cstr(&s);
+void nlang_postlude_print(nlangcp__nlang_stringmod_string s) {
+  const char *cstr = (const char *) nlang_stringmod_string_cstr(s);
   write(2, cstr, strlen(cstr));
 }
 
-void nlang_postlude_say(nlang_stringmod_string s) {
-  const char *cstr = (const char *) nlang_stringmod_string_cstr(&s);
+void nlang_postlude_say(nlangcp__nlang_stringmod_string s) {
+  const char *cstr = (const char *) nlang_stringmod_string_cstr(s);
   write(2, cstr, strlen(cstr));
   write(2, "\n", 1);
 }
